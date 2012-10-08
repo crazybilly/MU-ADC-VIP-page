ab911494fab268e190e3c321c5d7f70cecb82298
crazybilly <crazybilly@gmail.com>
2012-10-08 15:19:56 -0500

    Initial commit
    copied all files into the directory. Access online at:
    
    http://faculty.millikin.edu/~jtolbert/vip-mockup/new/MU-ADC-VIP-page/vip2.html

---
+++ vip.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+	/* when the page loads, hide the update thingy */
+	$('#update-inner').hide();
+	$('#update-work').hide();
+
+	/* toggle the whole update section */
+	$('#update-collapser').toggle (
+			function () {
+				$('#update-inner').slideDown();
+	$('#update-collapser').html("&uarr; Click to close this section &uarr;");
+				
+			}, function () {
+				$('#update-inner').slideUp();
+	$('#update-collapser').html("&darr; Click to update your record &darr;");
+			});
+
+
+	/* toggle the work section */
+	$('#work-collapser').toggle (
+			function () {
+				$('#update-work').slideDown();
+				$('#update-submit').addClass('open');
+				
+			}, function () {
+				$('#update-work').slideUp();
+				$('#update-submit').removeClass('open');
+			});
+
+}); /* close the document */
+
